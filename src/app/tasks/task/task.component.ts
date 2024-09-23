9e1fd29d26f3067bdc48462cbbc5322d4ca35ce3
cheikhtidianethiam <cheikhtidiane15.thiam@ucad.edu.sn>
2024-09-23 19:47:04 +0000

    the task list --

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { tasksComponent } from '../tasks.component';
+
+interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [tasksComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-
+  @Input({required: true}) task!: Task;
 }
