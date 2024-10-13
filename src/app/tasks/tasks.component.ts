c62ebf64d161561a39ebc7b090e281d7c602e8bf
cheikhtidianethiam <cheikhtidiane15.thiam@ucad.edu.sn>
2024-10-13 02:10:26 +0000

    Service

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from "./new-task/new-task.component";
 import { NewTask } from './task/task.model';
+import { Tasks } from './tasks.service';
 
 
 
@@ -15,40 +16,16 @@ import { NewTask } from './task/task.model';
 export class tasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  constructor(private tasks : Tasks){}
   isAdd=false;
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-16',
-    },
-  ];
+
 
   get selectedUserTasks() {
-
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasks.getUsers(this.userId);
   }
 
   OnCompleteTask(id:string){
-    this.tasks= this.tasks.filter((task)=> task.id !== id)
+    return this.tasks.OnRemoveTask(id)
   }
   OnAddTask(){
     this.isAdd=true;
@@ -57,13 +34,7 @@ export class tasksComponent {
     this.isAdd=false;
   }
   OnAddNewTask(taskData : NewTask){
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title:   taskData.title,
-      summary:taskData.summary,
-      dueDate: taskData.date,
-    });
+    this.tasks.OnAddTask(taskData,this.userId)
     this.isAdd=false
   }
 
